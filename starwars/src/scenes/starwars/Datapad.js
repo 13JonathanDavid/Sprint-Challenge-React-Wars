4dd89f0ccd51b2128a608a8822f0eb023350c7b2
Jonathan David <48808640+13JonathanDavid@users.noreply.github.com>
2019-04-26 16:19:16 -0400

    Update Datapad.js

--- starwars/src/scenes/starwars/Datapad.js
+++ starwars/src/scenes/starwars/Datapad.js
@@ -3,30 +3,39 @@ class Datapad extends Component{
    constructor(props){
         super(props);
         this.state = {
-            sourceData : props.sourceData,
             count: 0
         };
     
     }
     render(){
-        return (!this.state.sourceData.length)?"" :(
+        return (!this.props.sourceData.length)?"" :(
             <div className="datapad">
             <div className="vitals">
              <div className="vitals-header">
-               <div className="arrow-left"></div>
-             <h1> {this.state.sourceData.results[this.state.count].name}</h1>
-                 <div className="arrow-right"></div>
+               <div className="arrow-left" onClick={() => this.prev(this.state.count)}></div>
+             <h1 > {this.props.sourceData[this.state.count].name} </h1>
+                 <div className="arrow-right" onClick= {() => this.next(this.state.count)}></div>
              </div>
              <div className="vitals">
-               <h3> MASS: {this.state.sourceData.results[this.state.count].mass.toLowerCase() } </h3>
-               <h3> SKIN COLOR: {this.state.sourceData.results[this.state.count].skin_color.toLowerCase() } </h3>
-               <h3> EYE COLOR: {this.state.sourceData.results[this.state.count].eye_color.toLowerCase() } </h3>
-               <h3> Birth Year: {this.state.sourceData.results[this.state.count].birth_year.toLowerCase() } </h3>
+               <h3> MASS: {this.props.sourceData[this.state.count].mass.toLowerCase() } </h3>
+               <h3> SKIN COLOR: {this.props.sourceData[this.state.count].skin_color.toLowerCase() } </h3>
+               <h3> EYE COLOR: {this.props.sourceData[this.state.count].eye_color.toLowerCase() } </h3>
+               <h3> Birth Year: {this.props.sourceData[this.state.count].birth_year.toLowerCase() } </h3>
              </div>
                   
              </div>
              <img className="emblem"src="https://upload.wikimedia.org/wikipedia/commons/7/75/Emblem_of_the_First_Galactic_Empire.svg"/>
            </div>);
     };
+    next(x){
+        let len = this.props.sourceData.length;
+        if(x+1 < len)
+            this.setState({'sourceData':this.props.sourceData,'count':++x});
+        }
+    prev(x){
+        let len = this.props.sourceData.length;
+        if(x > 0)
+        this.setState({'sourceData':this.props.sourceData,'count':--x});
+    }
 }
 export default Datapad;
